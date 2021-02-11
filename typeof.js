00670c90a331c4b3c8a1532420f089edc0896cae
Nadezda <nadiapritchina@gmail.com>
2021-02-11 11:30:25 -0500

    Answer and examles about typeof

---
+++ typeof.js
@@ -0,0 +1,15 @@
+
+let nameH = "Angela";
+// name.slice(0,1);
+console.log(typeof (nameH));
+let numbers = 5;
+console.log(typeof (numbers));
+let boolN = true;
+console.log(typeof (boolN));
+
+let a = undefined;
+console.log(typeof (a));
+let b = { b: "c" };
+console.log(typeof (b));
+
+console.log(a);
